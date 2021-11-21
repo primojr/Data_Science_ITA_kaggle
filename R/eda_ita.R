5ffbe0f314c8c159cb40588b5f7195d888a3f6d7
Gerson <gesonprimo@gmail.com>
2021-11-21 14:07:00 -0300

    Preve analise exploratorias dos dados

--- R/eda_ita.R
+++ R/eda_ita.R
@@ -6,5 +6,13 @@ library(tidyverse)
 
 df <- read_csv("dados/warmupv4publictest.csv")
 
+# Conhecendo os dados
 glimpse(df)
+skimr::skim(df)
+
+df %>% DataExplorer::plot_histogram()
+df %>% select_if(., is.numeric) %>% 
+  DataExplorer::plot_correlation()
+
+# Obs: tratar os NA's
 
