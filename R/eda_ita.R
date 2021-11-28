7dbcc8894c73ef9366be195cffe4622bc3a75599
Gerson <gesonprimo@gmail.com>
2021-11-28 18:39:43 -0300

    Biblioteca tidymodels

--- R/eda_ita.R
+++ R/eda_ita.R
@@ -3,6 +3,7 @@
 
 # Carregar pacotes
 library(tidyverse)
+library(tidymodels)
 
 df <- read_csv("dados/warmupv4publictest.csv")
 
@@ -16,3 +17,4 @@ df %>% select_if(., is.numeric) %>%
 
 # Obs: tratar os NA's
 
+is.na(df) %>% colSums()
