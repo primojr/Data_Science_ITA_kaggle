82e962af1097f2a79c7e1b6508a1330382722e65
Gerson <gesonprimo@gmail.com>
2021-12-15 22:04:37 -0300

    Copia do workflow de modelagem do projeto Migração de dados BR

--- R/modelagem_ita.R
+++ R/modelagem_ita.R
@@ -1,10 +1,77 @@
-# Modelagem
+## 
+# Teste com outros algoritimos
+
 library(tidymodels)
+library(tidyverse)
+library(ranger)
+
+# 00. EDA Arvore
+df_tree %>% DataExplorer::plot_bar(.,by = 'classe'
+                                   ,title = 'classe ~ . '
+                                   ,ncol = 4)
 
 
+# 01.Splits
+splits <- initial_split(df_tree, strata = classe)
+br_trainning <- training(splits)
+br_testing <- testing(splits)
 
-#Step 1 - Split
+
+# 02.Pré processamento 
 
 
-#Step 2 - Pré processamento
+# 03.recipe
+reg_recipe <- recipe(
+  classe ~ . ,
+  data = br_trainning
+) %>% 
+  step_impute_knn(all_predictors()) %>% 
+  step_dummy(all_nominal_predictors())
 
+# 04. Engine
+reg_mod <- rand_forest(trees = tune()) %>% 
+  set_engine("ranger")  %>% 
+  set_mode("classification")
+
+# 05. Workflow
+reg_workflow <- workflow() %>% 
+  add_model(reg_mod) %>% 
+  add_recipe(reg_recipe)
+
+# 06.Cross validation
+val_set <- vfold_cv(br_trainning, v = 4, strata = classe)
+
+# 07.trainning
+reg_trained <- reg_workflow %>% 
+  tune_grid(
+    val_set,
+    grid = 5,
+    control = control_grid(save_pred = TRUE),
+    metrics = metric_set(accuracy)
+  )
+
+reg_trained %>% show_best()
+
+# autoplot
+ggplot2::autoplot(reg_trained)
+
+# see the magic
+reg_best_tune <- select_best(reg_trained, "rmse")
+final_reg_model <- reg_mod %>% 
+  finalize_model(reg_best_tune)
+
+workflow() %>% 
+  add_recipe(reg_recipe) %>% 
+  add_model(final_reg_model) %>% 
+  last_fit(splits) %>% 
+  collect_predictions() %>% 
+  select(.row, price, .pred) %>% 
+  ggplot() +
+  aes(x= price, y = .pred) +
+  geom_point()
+
+# save the results
+reg_fitted <- workflow() %>% 
+  add_recipe(reg_recipe) %>% 
+  add_model(final_reg_model) %>% 
+  fit(computers)   
